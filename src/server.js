65f04a2b17cb6d4ceeeda9fa119edb127d994622
George <64849154+ge5rg2@users.noreply.github.com>
2021-11-22 17:48:52 +0900

    templates
    before install Mongo DB

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
- const PORT = 4000;
+const PORT = 4000;
 
- const app = express();
+const app = express();
+const logger = morgan("dev");
 
- const handleListening = () =>
-   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
- app.listen(PORT, handleListening);
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
