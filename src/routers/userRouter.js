4939baf7279789e6445ec6ec54370a763ffedd50
George <64849154+ge5rg2@users.noreply.github.com>
2021-12-03 16:57:34 +0900

    user profile - 4
    change PW pt.1

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { postEdit, getEdit, logout, see, startGithubLogin, finishGithubLogin, } from "../controllers/userController";
+import {   getChangePassword,
+    postChangePassword, postEdit, getEdit, logout, see, startGithubLogin, finishGithubLogin, } from "../controllers/userController";
 import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter
+   .route("/change-password")
+   .all(protectorMiddleware)
+   .get(getChangePassword)
+   .post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
