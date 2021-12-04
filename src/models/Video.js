06258e5adfaee8a639fd310b277ddf2ff173accc
George <64849154+ge5rg2@users.noreply.github.com>
2021-12-05 02:18:11 +0900

    user profile - 7,8,9
    Files upload pt.2
    Static file (need to practice ES6)
    Video upload

--- src/models/Video.js
+++ src/models/Video.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
  const videoSchema = new mongoose.Schema({
    title: { type: String, required: true, trim: true, maxLength: 80 },
+   fileUrl: {type: String, required: true},
    description: { type: String, required: true, trim: true, minLength: 20 },
-    createdAt: { type: Date, required: true, default: Date.now },
+   createdAt: { type: Date, required: true, default: Date.now },
    hashtags: [{ type: String }],
    meta: {
     views: { type: Number, default: 0, required: true },
