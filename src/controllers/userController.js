4296723bcd09c11ff019cb1dba966a650cd21bea
George <64849154+ge5rg2@users.noreply.github.com>
2021-12-06 00:47:01 +0900

    setting style -1,2,3
    After challenge need to recap this part

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -199,7 +199,13 @@ export const postChangePassword = async (req, res) => {
 export const see = async (req, res) => {
   const {id} = req.params;
   // session에서 찾지 않고 url에서 찾는 이유는 다른 방문자도 볼 수 있도록 하기 위함.
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", {pageTitle: "User not found"});
   }
