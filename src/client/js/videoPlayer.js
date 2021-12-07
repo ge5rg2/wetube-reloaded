b3ddf77037677562fec249a16acda6650e2bec83
George <64849154+ge5rg2@users.noreply.github.com>
2021-12-07 17:42:34 +0900

    video player- 4,5
    duration and current time
    Time formatting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,8 +2,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volumeRange");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -46,6 +47,22 @@ const handleVolumeChange = (event) => {
     video.volume = value;
 }
 
+const formatTime = (seconds) =>
+    new Date(seconds * 1000).toISOString().substr(14, 5);
+    // new date()는 1970.01.01...으로 시작한다 toISOString는 이것을 스트링화 시키고, substrs은 (ㄱ,ㄴ) ㄱ: 앞에서부터 자르는 숫자 ㄴ: 뒤에서부터 자르는 숫자
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = () => {
+    currenTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+// eventlistener에서 input 이벤트로 range조절
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+// 재생 시간관련 이벤트들
