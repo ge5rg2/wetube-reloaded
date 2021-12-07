1c45b7b69191343d21ac555443fcd178566cda1c
George <64849154+ge5rg2@users.noreply.github.com>
2021-12-07 13:31:23 +0900

    video player - 2,3
    Mute Unmute
    volume range

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,7 +3,10 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volumeRange");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
 console.log("video click")
@@ -12,13 +15,37 @@ console.log("video click")
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMute = (e) => {};
+const handleMuteClick = (e) => {
+    if (video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+    const {
+        target: { value },
+    } = event;
+    if (video.muted || value != 0) {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+        muteBtn.disabled = false;
+    }
+    if (value == 0){
+        video.mute = true;
+        muteBtn.innerText = "Unmute";
+        muteBtn.disabled = true;
+    }
+    volumeValue = value;
+    video.volume = value;
+}
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
