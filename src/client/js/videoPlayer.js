18deae2a3bd9128c44a76c738d2c45567b89a952
George <64849154+ge5rg2@users.noreply.github.com>
2021-12-08 17:03:51 +0900

    video player -6,7,8,9,10,11
    Timeline
    Fullscreen
    Controls Events
    styles pt.1

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,22 +1,30 @@
 
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volumeRange");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenBtnIcon = fullScreenBtn.querySelector("i");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
-console.log("video click")
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMuteClick = (e) => {
@@ -25,7 +33,7 @@ const handleMuteClick = (e) => {
     } else {
         video.muted = true;
     }
-    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    muteBtnIcon.classList = video.muted ? "fas fa-volume-up" : "fas fa-volume-mute";
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -35,13 +43,12 @@ const handleVolumeChange = (event) => {
     } = event;
     if (video.muted || value != 0) {
         video.muted = false;
-        muteBtn.innerText = "Mute";
-        muteBtn.disabled = false;
+        muteBtnIcon.classList = "fas fa-volume-mute";
+        muteBtnIcon.disabled = false;
     }
     if (value == 0){
         video.mute = true;
-        muteBtn.innerText = "Unmute";
-        muteBtn.disabled = true;
+        muteBtnIcon.classList = "fas fa-volume-up";
     }
     volumeValue = value;
     video.volume = value;
@@ -53,10 +60,71 @@ const formatTime = (seconds) =>
 
 const handleLoadedMetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
     currenTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+    //타임라인의 value를 실시간 비디오 시간에 맞춘다
+};
+
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+const handleFullscreen = () => {
+  const fullscreen = document.fullscreenElement;
+    //현재 전체화면인지 알 수 있게해주는 함수 true false로 나뉨
+    if (fullscreen) {
+      document.exitFullscreen();
+    } else {
+      videoContainer.requestFullscreen();
+    }
+};
+
+const autoScreenChange = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    fullScreenBtnIcon.classList = "fas fa-compress";
+  } else {
+    fullScreenBtnIcon.classList = "fas fa-expand";
+  }
+}
+// 전체화면 전환시 아이콘 변경 
+
+const playKey = (e) => {
+    if(e.which === 32) {
+        handlePlayClick();
+    } else if(e.which === 70) {
+      handleFullscreen();
+    } else if(e.which === 77) {
+      handleMuteClick(e);
+    }
+};
+
+const hideControls = () => videoControls.classList.remove("showing");
+
+const handleMouseMove = () => {
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsTimeout = null;
+  }
+  videoControls.classList.add("showing");
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
+};
+
+const handleMouseLeave = () => {
+  controlsTimeout = setTimeout(() => {
+    videoControls.classList.remove("showing");
+  }, 3000);
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -66,3 +134,10 @@ volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 // 재생 시간관련 이벤트들
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullscreen);
+window.addEventListener("keydown", playKey);
+//키보드 이벤트
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
+document.addEventListener("fullscreenchange", autoScreenChange);
